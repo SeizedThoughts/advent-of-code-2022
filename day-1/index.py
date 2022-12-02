4ab319c20af199a17a44dfda6b7747542c8f57b7
SeizedThoughts <seizedthoughts@gmail.com>
2022-12-01 21:23:18 -0800

    This made no sense.

--- day-1/index.py
+++ day-1/index.py
@@ -1,4 +1,14 @@
-input = [(i) for i in open('day-n/input.txt', 'r').read().split('\n')]
+input = [(i.split('\n')) for i in open('day-1/input.txt', 'r').read().split('\n\n')]
 
+m = []
 for i in range(len(input)):
-    line = input[i]
+    elf = input[i]
+    n = 0
+    for j in range(len(elf)):
+        cal = int(elf[j])
+        n += cal
+    m.append(n)
+
+
+
+print(sum(sorted(m)[-3::]))
